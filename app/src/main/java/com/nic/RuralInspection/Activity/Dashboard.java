698d939471823be8919e4224c25efd8439f90ec1
NIC1 <preethikabalu@gmail.com>
2018-12-31 15:23:15 +0530

    Add dummy commit in dashboard

--- app/src/main/java/com/nic/RuralInspection/Activity/Dashboard.java
+++ app/src/main/java/com/nic/RuralInspection/Activity/Dashboard.java
@@ -13,6 +13,8 @@ public class Dashboard extends AppCompatActivity {
     @Override
     public void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState );
+
+
         setContentView(R.layout.activity_main);
     }
 }
