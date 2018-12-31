a00d386d4c53ccba0a8cb2e6e1759572fb6a8679
NIC1 <preethikabalu@gmail.com>
2018-12-31 16:24:20 +0530

    undo dummy commit

--- app/src/main/java/com/nic/RuralInspection/Activity/Dashboard.java
+++ app/src/main/java/com/nic/RuralInspection/Activity/Dashboard.java
@@ -13,8 +13,6 @@ public class Dashboard extends AppCompatActivity {
     @Override
     public void onCreate( Bundle savedInstanceState ) {
         super.onCreate(savedInstanceState );
-
-
         setContentView(R.layout.activity_main);
     }
 }
