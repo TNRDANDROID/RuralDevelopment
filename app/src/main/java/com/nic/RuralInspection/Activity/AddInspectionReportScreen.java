fd3e51fc1cea773b613cb613929348c6017175d7
AchanthiSundar <m.s.achanthisundar@gamil.com>
2019-03-07 13:04:05 +0530

    dummm

--- app/src/main/java/com/nic/RuralInspection/Activity/AddInspectionReportScreen.java
+++ app/src/main/java/com/nic/RuralInspection/Activity/AddInspectionReportScreen.java
@@ -90,6 +90,8 @@ public class AddInspectionReportScreen extends AppCompatActivity implements View
     private List<View> viewArrayList = new ArrayList<>();
     PrefManager prefManager;
 
+
+
     private static Context context;
 
     final int CAMERA_REQUEST = 1888;
